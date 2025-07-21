2ca3e3b13502e1cc104a2a9f4e2c706a83411e4d
PranjalGade <pranjal.gadebmt@gmail.com>
2025-07-21 23:19:19 +0530

    Add files via upload

---
+++ py/Roundoff.py
@@ -0,0 +1,4 @@
+num=float(input("Enter the number : "))
+dec=int(input("Enter the decimals you want to round off : "))
+round_num=round(num,dec)
+print(round_num)
---
+++ py/sqrt_curt_no.py
@@ -0,0 +1,9 @@
+def Calculate(num):
+    Square_root=num**(0.5)
+    cube_root=num**(1/3)
+    print(f"Numbers : {num}")
+    print(f"Square root : {Square_root}")
+    print(f"Cube root : {cube_root}")
+
+num=int(input("Enter the  number :"))
+Calculate(num)
