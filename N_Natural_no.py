c726d16f738c9e047b77c68447d745519a5e9e71
PranjalGade <pranjal.gadebmt@gmail.com>
2025-07-21 23:23:58 +0530

    Delete N_Natural_no.py

--- N_Natural_no.py
+++
@@ -1,7 +0,0 @@
-n=int(input("Enter a number : "))
-if n<=0:
-    print("Enter positive number")
-else:
-    print(f"The first {n} natural numbers are :")
-    for i in range(1,n+1) :
-        print(i)         
